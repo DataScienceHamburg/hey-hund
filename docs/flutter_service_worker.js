445da0b2324597ae017d0da9788444b93a4f153f
bert.gollnick <bert.gollnick@posteo.net>
2024-06-09 17:33:31 +0200

    changes for table screensize

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -2182,9 +2182,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "11c7a0296523fcc6d8d3bd02452639ae
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "12434bc4111a62374823c3fac72215f0",
-"/": "12434bc4111a62374823c3fac72215f0",
-"main.dart.js": "ed446670f364d0338b70621d06e3e111",
+"index.html": "04deeaef513ed8289006f4a7de643f4b",
+"/": "04deeaef513ed8289006f4a7de643f4b",
+"main.dart.js": "111d9213a113071c195b1f1871bc69c9",
 "manifest.json": "18b05de58f5aeed2ffa6be1246cc43f1",
 "version.json": "53fb59e54fb87bb44dba7c2200e1dead"};
 // The application shell files that are downloaded before a service worker can
