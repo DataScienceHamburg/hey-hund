b8234b4b45e2de724554c4394e4392b46b30ec76
bert.gollnick <bert.gollnick@posteo.net>
2024-06-25 10:13:39 +0200

    google tag added

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -2214,8 +2214,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "66615dc268d293e9d84f71eddc88a893
 "icons/Icon-512.png": "c3d006edb406cea68b7e7dbef89ebd0b",
 "icons/Icon-maskable-192.png": "da5e5ee1fc61ea6bcbe48a1fb30380c0",
 "icons/Icon-maskable-512.png": "734b3c1783520411773e2354c96ed624",
-"index.html": "ae116f47a2bf2acae4bdb6680d851d5c",
-"/": "ae116f47a2bf2acae4bdb6680d851d5c",
+"index.html": "1a189941f2bc6204bb425da4c4d72de3",
+"/": "1a189941f2bc6204bb425da4c4d72de3",
 "main.dart.js": "d450f91dcefaecb9881851f0f70ee4cc",
 "manifest.json": "6a12b642b2f15b49fac7d6ccf5a722af",
 "version.json": "53fb59e54fb87bb44dba7c2200e1dead"};
